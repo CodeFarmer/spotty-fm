a4f84a6080f9022fb9686cc913ee43ecb76fc4e1
Joel Gluth <joel@gluth.io>
2018-12-29 23:53:37 +0000

    Upgraded http-kit to dev version because stable does not work with JDK11

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.json "0.2.6"]
                  [environ "1.1.0"]
-                 [http-kit "2.3.0"]]
+                 [http-kit "2.4.0-alpha2"]] ;; http-kit 2.3.0 has problems with jdk11
 
   :main ^:skip-aot spotty-fm.main
   :target-path "target/%s"
