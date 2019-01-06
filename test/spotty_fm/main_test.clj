4405d42cab0d49410c642a84a02f166c31ffeebf
Joel Gluth <joel@gluth.io>
2019-01-06 00:16:30 +0000

    Neglected to add main test to git

---
+++ test/spotty_fm/main_test.clj
@@ -0,0 +1,23 @@
+(ns spotty-fm.main-test
+  (:require [clojure.test :refer :all]
+            [spotty-fm.main :refer :all]
+            [spotty-fm.core :refer [config]]
+            [spotty-fm.spotify :as spotify]))
+
+
+(deftest test-lastfm-to-spotify
+
+  (let [t (:access_token (spotify/fetch-client-auth-token (:clientid (:spotify config))
+                                                          (:secret (:spotify config))))]
+    (testing "Known lastfm track to known spotify track"
+      
+      (let [spotify-track (lastfm-to-spotify t {:title "Any Day Now",
+                                                :artist "Elbow",
+                                                :mbid "f93eff49-4068-4074-99a9-1b61ea06f530"})]
+        
+        (is (= spotify-track {:title "Any Day Now"
+                              :artist "Elbow"
+                              :isrc "GBBLK0000224"})
+            "Known spotify track should be returned for known lastfm track")))))
+
+
